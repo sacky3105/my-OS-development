e44883eac62e5b620d1314cb1376894146551ebb
sacky3105 <kgglee.114.baritone@gmail.com>
2024-06-17 23:07:17 +0900

    refacter OS basic

---
+++ 04_days/harib01d/bootpack.c
@@ -0,0 +1,17 @@
+//　bootpack.c
+
+extern void io_hlt(void);
+
+void HariMain(void) 
+{
+  char *p;
+  p = (char *) 0xa0000;
+
+  for(int i = 0; i <= 0xaffff; i++){
+    *(p + i) = i & 0x0f;
+  }
+
+  for(;;){
+    io_hlt();
+  }
+}
